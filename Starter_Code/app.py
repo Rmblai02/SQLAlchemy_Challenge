7ef31ba23542b150277f7359d6fd49cb08dc17fb
Ryan Blais <131097736+Rmblai02@users.noreply.github.com>
2023-07-17 18:29:13 -0500

    Updates

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -1,30 +1,101 @@
 # Import the dependencies.
+import numpy as np
 
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
 
 
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
-
+Base= automap_base()
 # reflect the tables
-
+Base.prepare(autole_engine=engine, reflect=True)
 
 # Save references to each table
-
+Measurements = Base.classes.measurements
+Station= Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session= Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
 
-
+app = Flask(__name__)
 
 
 #################################################
 # Flask Routes
 #################################################
+#create root route
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+        f"Note: the start date and the end date are both in the format yyyy-mm-dd"
+    )
+
+session.close()
+
+#precipitation route
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all precipitation data
+    precipitation_results = session.query(Measurements.date, Measurements.prcp).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    prcp_list = list(np.ravel(precipitation_results))
+
+    return jsonify(prcp_list)
+
+#stations route
+@app.route("/api/v1.0/stations")
+def stations():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all station data
+    station_results = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    station_list = list(np.ravel(station_results))
+
+    return jsonify(station_list)
+
+#tobs route
+@app.route("/api/v1.0/tobs")
+def tobs():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    # Query all tobs data
+    tobs_results = session.query(Measurements.date, Measurements.tobs).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    tobs_list = list(np.ravel(tobs_results))
+
+    return jsonify(tobs_list)
