69c5fe12a1d93f4957e227a11e843ff4fe3b3096
Ryan Blais <131097736+Rmblai02@users.noreply.github.com>
2023-07-17 21:48:40 -0500

    Updates

--- Starter_Code/app.py
+++ Starter_Code/app.py
@@ -17,7 +17,7 @@ engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 # reflect an existing database into a new model
 Base= automap_base()
 # reflect the tables
-Base.prepare(autole_engine=engine, reflect=True)
+Base.prepare(engine, reflect=True)
 
 # Save references to each table
 Measurements = Base.classes.measurements
@@ -47,55 +47,12 @@ def welcome():
         f"/api/v1.0/tobs<br/>"
         f"/api/v1.0/<start><br/>"
         f"/api/v1.0/<start>/<end><br/>"
-        f"Note: the start date and the end date are both in the format yyyy-mm-dd"
     )
 
 session.close()
 
 #precipitation route
-@app.route("/api/v1.0/precipitation")
-def precipitation():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
 
-    # Query all precipitation data
-    precipitation_results = session.query(Measurements.date, Measurements.prcp).all()
 
-    session.close()
-
-    # Convert list of tuples into normal list
-    prcp_list = list(np.ravel(precipitation_results))
-
-    return jsonify(prcp_list)
-
-#stations route
-@app.route("/api/v1.0/stations")
-def stations():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    # Query all station data
-    station_results = session.query(Station.station, Station.name, Station.latitude, Station.longitude, Station.elevation).all()
-
-    session.close()
-
-    # Convert list of tuples into normal list
-    station_list = list(np.ravel(station_results))
-
-    return jsonify(station_list)
-
-#tobs route
-@app.route("/api/v1.0/tobs")
-def tobs():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    # Query all tobs data
-    tobs_results = session.query(Measurements.date, Measurements.tobs).all()
-
-    session.close()
-
-    # Convert list of tuples into normal list
-    tobs_list = list(np.ravel(tobs_results))
-
-    return jsonify(tobs_list)
+if __name__ == '__main__':
+    app.run(debug=True) 
